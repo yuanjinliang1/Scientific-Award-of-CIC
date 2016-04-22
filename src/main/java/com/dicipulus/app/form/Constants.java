a65eda523d90dd23f7f09b7311bbbf4d0448bc4a
chenyouqi <chenyouqi_123@126.com>
2016-04-22 14:52:20 +0800

    4-22

---
+++ src/main/java/com/dicipulus/app/form/Constants.java
@@ -0,0 +1,5 @@
+package com.dicipulus.app.form;
+
+public class Constants {
+	
+}
---
+++ src/main/java/com/dicipulus/app/form/Unit.java
@@ -0,0 +1,51 @@
+package com.dicipulus.app.form;
+
+/**
+ * @author chenyouqi
+ *完成单位
+ */
+public class Unit {
+	String unitName;
+	String applyTechnique;
+	String startTame;
+	String contacts;
+	String phoneNumber;
+	String information;
+	public String getUnitName() {
+		return unitName;
+	}
+	public void setUnitName(String unitName) {
+		this.unitName = unitName;
+	}
+	public String getApplyTechnique() {
+		return applyTechnique;
+	}
+	public void setApplyTechnique(String applyTechnique) {
+		this.applyTechnique = applyTechnique;
+	}
+	public String getStartTame() {
+		return startTame;
+	}
+	public void setStartTame(String startTame) {
+		this.startTame = startTame;
+	}
+	public String getContacts() {
+		return contacts;
+	}
+	public void setContacts(String contacts) {
+		this.contacts = contacts;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getInformation() {
+		return information;
+	}
+	public void setInformation(String information) {
+		this.information = information;
+	}
+	
+}

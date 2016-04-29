b8ba86cb7199df260cd66e9488ad8716e2bf9521
daisongtai <daisongtai@gmail.com>
2016-04-29 17:34:32 +0800

    -添加第四个表格的jdbc与model，还需要写view与controller -写了jdbc实例化的静态函数,放在InitJdbc类里面，可以直接调用 -项目组选择创建推荐书类型的时候就建立所有表格，通过调用createAllForms(Applier applier)函数来实现， 以后增加一个表格的Jdbc，只要在CreateAllForms类中增加相应的create函数即可。 -项目组被删除时，先删除项目组所有表格信息，再删除项目组的记录。通过DeleteAllForms实现

---
+++ src/main/java/com/dicipulus/app/JDBC/DeleteFormsJdbc.java
@@ -0,0 +1,52 @@
+package com.dicipulus.app.JDBC;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import javax.sql.DataSource;
+
+import com.dicipulus.app.applicationModel.FirstProjectBasicSituationTA;
+import com.dicipulus.app.model.Applier;
+import com.dicipulus.app.model.Referee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class DeleteFormsJdbc{
+	private static final Logger logger=LoggerFactory.getLogger(CreateFormsJdbc.class);
+	private DataSource dataSource;
+	
+	private JdbcTemplate jdbcTemplateObject;
+		
+	public void setDataSource(DataSource dataSource){
+		this.dataSource=dataSource;
+		this.jdbcTemplateObject= new JdbcTemplate(dataSource);
+	}
+	
+	public void deleteAllForms(Applier applier){
+		deleteProjectMajor(applier);
+		deleteSecondRefereeUnitOpinion(applier);
+	}
+	
+	public void deleteProjectMajor(Applier applier){
+		String sql="delete from project_major where applierUid=? ";
+		jdbcTemplateObject.update(sql,applier.getUid());
+		logger.info(sql);
+	}
+	
+	public void deleteSecondRefereeUnitOpinion(Applier applier){
+		String sql="delete from secondrefereeunitopinion where applierUid=? ";
+		jdbcTemplateObject.update(sql,applier.getUid());
+		logger.info(sql);
+	}
+}
+	
---
+++ src/main/java/com/dicipulus/app/JDBC/FourthFormJdbc.java
@@ -0,0 +1,45 @@
+package com.dicipulus.app.JDBC;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import javax.sql.DataSource;
+
+import com.dicipulus.app.applicationModel.*;
+import com.dicipulus.app.model.Applier;
+import com.dicipulus.app.model.Referee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FourthFormJdbc{
+	private static final Logger logger=LoggerFactory.getLogger(RefereeJdbc.class);
+	private DataSource dataSource;
+	
+	private JdbcTemplate jdbcTemplateObject;
+		
+	public void setDataSource(DataSource dataSource){
+		this.dataSource=dataSource;
+		this.jdbcTemplateObject= new JdbcTemplate(dataSource);
+	}
+	
+	public FourthForm getFourthForm(String applierUid){
+		String sql="select * form project_major where applierUid=?;";
+		FourthForm fourthForm=jdbcTemplateObject.queryForObject(sql, new Object[]{applierUid},
+				BeanPropertyRowMapper.newInstance(FourthForm.class));
+		logger.info("SQL: "+sql);
+		return fourthForm;
+	}
+	
+	public void setFourthForm(FourthForm fourthForm, String applierUid){
+		String sql="update project_major set fourthForm1=?, fourthForm2=? where applierUid=?;";
+		jdbcTemplateObject.update(sql,fourthForm.getFourthForm1(),fourthForm.getFourthForm2(),applierUid);
+		logger.info("SQL: "+sql);
+	}
+}

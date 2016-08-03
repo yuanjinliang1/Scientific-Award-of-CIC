199b47ed473de3ba12004e4f73f39dff1595cd5f
daisongtai <daisongtai@gmail.com>
2016-08-03 16:20:10 +0800

    -附件上传后的按字符排序

--- src/main/java/com/dicipulus/app/fileController/DelteFiles.java
+++ src/main/java/com/dicipulus/app/fileController/DelteFiles.java
@@ -1,6 +1,7 @@
 package com.dicipulus.app.fileController;
 
 import java.io.File;
+import java.util.Arrays;
 
 import org.apache.commons.io.FileUtils;
 
@@ -9,6 +10,7 @@ public final class DelteFiles {
 	public static void deleteFiles(String pathNow){
 		File folder = new File(pathNow);
 		File[] listOfFiles = folder.listFiles();
+		Arrays.sort(listOfFiles);
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile()) {
 				boolean isSuccess = org.springframework.util.FileSystemUtils
@@ -22,6 +24,7 @@ public final class DelteFiles {
 	public static void deleteSingleFiles(String pathNow,int fileLead){
 		File folder = new File(pathNow);
 		File[] listOfFiles = folder.listFiles();
+		Arrays.sort(listOfFiles);
 		File fileToDelete=listOfFiles[fileLead];
 			if (fileToDelete.isFile()) {
 				boolean isSuccess = org.springframework.util.FileSystemUtils
